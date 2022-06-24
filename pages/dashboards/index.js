c1d3b3b0bc5a92e7997dc35cf2a8142ec88887e4
vtVasconcelos <victor.vasconcelos@conversion.com.br>
2022-06-24 15:14:44 -0300

    feat: limpa código

--- pages/dashboards/index.js
+++ pages/dashboards/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
+import privateRoute from '../api/privateRoute'
 
-export default function Home() {
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,9 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          SearchHUB <a href="http://conversion.com.br/">| Conversion</a>
+          SearchHUB <a href="https://conversion.com.br/">| Conversion</a>
         </h1>
       </main>
     </div>
   )
 }
+
+export default privateRoute(Home)
--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Login() {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           SearchHUB <a href="http://conversion.com.br/">| Conversion</a>
         </h1>
+        <h2 className={styles.subtitle}>Faça <a href="#">login</a> para continuar, ou se preferir, <a href="/">volte ao inicio</a></h2>
       </main>
     </div>
   )
