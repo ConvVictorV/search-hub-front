36b326f42449ee6165e883dfb34327d6bc3d7f98
root <root@DESKTOP-MVIUKGR.localdomain>
2022-07-06 14:35:04 -0300

    fix: corrige pagina de inicio

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,7 @@ import DefaultLayout from '../Layouts/default'
 import styles from '../styles/Home.module.css'
 function Home() {
   return (
-    <DefaultLayout title="Home | SearchHub" description="SearchHub Conversion">
-    <h1 className={styles.title}>
-      SearchHUB <a href="http://conversion.com.br/" target={'_blank'} rel={"noreferrer"}>| Conversion</a>
-    </h1>
+    <DefaultLayout toggleTheme={args.toggleTheme} background={1} pageName="Inicio" >
     </DefaultLayout>
   )
 }
