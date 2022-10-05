0462668d4e6b25ba167fd04eee2b31cc0e05c4ab
vtVasconcelos <victor.vasconcelos@conversion.com.br>
2022-10-05 16:18:07 -0300

    fix: teste

--- pages/api/get/words.js
+++ pages/api/get/words.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   return axios
     .get(`${process.env.BACKENDHOST}/words/${idcustomer || ""}`)
     .then(({ data }) => res.status(200).send(data))
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err.data));
 }
 
 export const config = {
