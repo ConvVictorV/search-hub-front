54bc7f7a35d81e3983e1a3c0dea5e10211fd46ec
Gustavo Sales <gustavo.sales@conversion.com.br>
2022-11-11 10:50:21 -0300

    fix: Corrige api para link de produção

--- pages/api/get/qwPackages.js
+++ pages/api/get/qwPackages.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export default function handler(req, res) {
   const { idcustomer, page } = req.query || {};
   return axios
-    //.get(`https://search-hub-backend-nukcfjbsza-rj.a.run.app/qwpackages/${idcustomer || ""}?items=2000&page=${page}`)
-    .get(`http://localhost:3005/qwpackages/${idcustomer || ""}?items=2000&page=${page}`)
+    .get(`https://search-hub-backend-nukcfjbsza-rj.a.run.app/qwpackages/${idcustomer || ""}?items=2000&page=${page}`)
     .then(({ data }) => res.status(200).send(data))
     .catch((err) => res.status(500).send(err.data));
 }
