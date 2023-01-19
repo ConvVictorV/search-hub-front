ea9a08501e1635143e45552a22e3467115697c39
Gustavo Sales <gustavo.sales@conversion.com.br>
2023-01-19 15:10:29 -0300

    feat: envia info sobre usuario nas requests

--- pages/api/post/textTopic.js
+++ pages/api/post/textTopic.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default function handler(req, res) {
   const backend = process.env.ENV == 'DEV' ? 'https://search-hub-backend-homolog-nukcfjbsza-rj.a.run.app' : 'https://search-hub-backend-nukcfjbsza-rj.a.run.app'
+  axios.defaults.headers.common['useremail'] = req.headers.useremail || 'não definido'
   axios
     .post(`${backend}/texttopics`, req.body)
     .then(({ data }) => res.status(200).send(data))
