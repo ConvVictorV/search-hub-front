af1fa86384f46bf425a42ae8ad603eed6c294180
vtVasconcelos <victor.vasconcelos@conversion.com.br>
2023-01-31 10:32:33 -0300

    fix: corrige export sheet

--- pages/api/get/exportQuickWins.js
+++ pages/api/get/exportQuickWins.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 const fastcsv = require("fast-csv");
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { idcustomer } = req.query || {};
   const { content } = req.body || {};
   let csv = "";
   if (content) {
-    axios.post('https://script.google.com/macros/s/AKfycbwS75r9vSNlU4U8vjp7PDquAkaoOsgIyHW-_F33ZiLbQRrX4GdIOnauCoLBcFQ2f001vQ/exec', content).then(({data})=>{
-      return res.status(200).send({data})
+    axios.post('https://script.google.com/macros/s/AKfycbwS75r9vSNlU4U8vjp7PDquAkaoOsgIyHW-_F33ZiLbQRrX4GdIOnauCoLBcFQ2f001vQ/exec', content).then(({ data }) => {
+      return res.status(200).send({ data })
     })
   }
   else {
-  return axios
-    .get(`${process.env.BACKENDHOST}/quickwins/${idcustomer || ""}`)
-    .then(({ data }) => {
-      fastcsv
-        .write(data, { headers: true })
-        .on("data", (row) => {
-          csv += row;
-        })
-        .on("end", () => res.status(200).send(csv));
-    })
-    .catch((err) => res.status(500).send(err));
+    return axios
+      .get(`${process.env.BACKENDHOST}/quickwins/${idcustomer || ""}`)
+      .then(({ data }) => {
+        fastcsv
+          .write(data, { headers: true })
+          .on("data", (row) => {
+            csv += row;
+          })
+          .on("end", () => res.status(200).send(csv));
+      })
+      .catch((err) => res.status(500).send(err));
   }
 }
